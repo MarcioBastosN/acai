fcd45ec82d87bf637a6c111ee79a53cba2feeda4
MarcioBastosN <marciobastosn@gmail.com>
2023-10-25 11:05:54 -0300

    ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Açai',
       theme: lightTheme,
       darkTheme: darkTheme,
       initialRoute: AppRoutes.home,
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,9 @@
+import 'dart:ffi';
+
+import 'package:acai/model/login.dart';
+import 'package:acai/view/component/text_controller_component.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -10,16 +15,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final Login l = Get.put(Login());
 
   @override
   Widget build(BuildContext context) {
+    final fText = FocusNode();
+
+    l.valida();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
@@ -29,18 +32,37 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              "teste variavel ${l.user.value.text.toString()}",
+            ),
+            MyCustonText(
+              controller: l.user.value,
+              label: "Nome",
+              inputType: TextInputType.text,
+              focus: fText,
+              onTapOutside: (event) {
+                FocusScope.of(context).unfocus();
+                l.valida();
+              },
+              perfixIcon: Icons.person,
+            ),
+            MyCustonText(
+              controller: l.password.value,
+              label: "Senha",
+              inputType: TextInputType.visiblePassword,
+              onTapOutside: (event) {
+                FocusScope.of(context).unfocus();
+                l.valida();
+              },
+              obscureText: true,
+              perfixIcon: Icons.key_off,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        backgroundColor: l.valide.value == true ? Colors.green : Colors.red,
+        onPressed: () => {},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
